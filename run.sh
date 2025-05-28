7d009a216756acc4e21e721248a12f2c5ad0ef2a
thanh5320 <thanh20183632@gmail.com>
2025-05-28 09:22:22 +0700

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-source /home/thanhnv/Desktop/STOCK/stock-noti/env/bin/activate
-python3 /home/thanhnv/Desktop/STOCK/stock-noti/main.py
+#source /home/thanhnv/Desktop/STOCK/stock-noti/env/bin/activate
+#python3 /home/thanhnv/Desktop/STOCK/stock-noti/main.py
+
+source /root/thanhnv/stock/env/bin/activate
+python3 /root/thanhnv/stock/main.py
