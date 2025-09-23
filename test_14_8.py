49ff7edd68809da73f3c6360b467a558b5b93fdd
thanh5320 <thanh20183632@gmail.com>
2025-09-24 00:49:20 +0700

    update

---
+++ test_14_8.py
@@ -0,0 +1,47 @@
+# from vnstock import Trading
+import pandas as pd
+from sqlalchemy import create_engine, text
+import time
+#
+# pd.set_option("display.max_rows", None)
+# pd.set_option("display.max_columns", None)
+# pd.set_option("display.max_colwidth", None)
+# trading = Trading(source='VCI')
+# x = trading.price_board(symbols_list=['MSN']).T
+# print(x)
+
+from vnstock import Quote
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_colwidth", None)
+quote = Quote(symbol='MSN', source='VCI',)
+df = quote.intraday(page_size= 15000, show_log=False)
+# # x = quote.intraday(symbol='MSN')
+# print(x)df
+# engine = create_engine("postgresql+psycopg2://root:root@207.180.246.67:5432/trading_stock")
+engine = create_engine("postgresql+psycopg2://root:root@127.0.0.1:5432/trading_stock")
+
+def insert_intraday(df, batch_size=1000):
+    # print("Inserting")
+    insert_sql = """
+    INSERT INTO intraday (id, stock, time, price, volume, match_type)
+    VALUES (:id, :stock, :time, :price, :volume, :match_type)
+    ON CONFLICT (id, stock, time) DO NOTHING;
+    """
+    with engine.begin() as conn:
+        for start in range(0, len(df), batch_size):
+            print("Inserting")
+            batch = df.iloc[start:start+batch_size]
+            conn.execute(text(insert_sql), batch.to_dict(orient="records"))
+
+df["stock"] = "MSN"
+
+# Chuyển time sang datetime (timezone aware -> UTC nếu cần)
+# df["time"] = pd.to_datetime(df["time"])
+# df["time"] = df["time"].dt.tz_convert("UTC")
+df["time"] = df["time"].dt.tz_convert("Asia/Ho_Chi_Minh")
+
+insert_intraday(df, batch_size=1000)
+# print(df)
+
+print("Insert completed (duplicates skipped).")
